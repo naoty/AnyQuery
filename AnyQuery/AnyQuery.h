148ae5d215b3f89ca4b5afbfe70a3e89436d729c
Naoto Kaneko <naoty.k@gmail.com>
2016-02-23 21:07:24 +0900

    Initial commit

---
+++ AnyQuery/AnyQuery.h
@@ -0,0 +1,19 @@
+//
+//  AnyQuery.h
+//  AnyQuery
+//
+//  Created by Naoto Kaneko on 2/23/16.
+//  Copyright © 2016 Naoto Kaneko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AnyQuery.
+FOUNDATION_EXPORT double AnyQueryVersionNumber;
+
+//! Project version string for AnyQuery.
+FOUNDATION_EXPORT const unsigned char AnyQueryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AnyQuery/PublicHeader.h>
+
+
